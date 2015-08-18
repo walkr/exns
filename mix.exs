bbbf6ba62476c459e38e1bafa7ffb33b56a1bf61
walkr <walkr@local>
2015-08-18 00:07:21 -0700

    Update mix file

--- mix.exs
+++ mix.exs
@@ -5,10 +5,9 @@ defmodule Exns.Mixfile do
     [app: :exns,
      version: "0.0.1-alpha",
      elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
      deps: deps,
-     description: description]
+     description: description,
+     package: package]
   end
 
   # Configuration for the OTP application
