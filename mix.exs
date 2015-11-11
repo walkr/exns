5a8cf48387dd7c4a22651262bcf2ca6a6906ccd5
walkr <walkr@local>
2015-11-11 14:55:41 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exns.Mixfile do
 
   def project do
     [app: :exns,
-     version: "0.2.0-beta",
+     version: "0.3.0-beta",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
